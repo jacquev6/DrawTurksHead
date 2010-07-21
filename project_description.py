ea2340c939ea0b331873ffb27c37cf6b7e879a9d
Vincent Jacques <vincent@vincent-jacques.net>
2010-07-21 15:12:04 +0200

    Add an empty Python module

--- project_description.py
+++ project_description.py
@@ -9,6 +9,20 @@ DynamicLibrary(
     externalLibraries = [ "cairomm-1.0" ]
 )
 
+CppPythonModule(
+    name = "turkshead._turkshead",
+    sources = AllCppIn( "src/PythonModule" ),
+    localLibraries = [ "turkshead" ],
+    externalLibraries = [ "boost_python", "python2.7" ]
+)
+
+PythonPackage(
+    name = "turkshead_package",
+    pythonPythonModules = AllPyIn( "src/PythonModule" ),
+    stripFromModules = lambda s: s.replace( "src/PythonModule", "turkshead" ),
+    cppPythonModules = [ "turkshead._turkshead" ]
+)
+
 ### @todo Delete this executable as soon as a Python script can drive the turkshead library
 Executable(
     name = "draw_turks_head",
---
+++ src/PythonModule/module.cpp
@@ -0,0 +1,19 @@
+#include <boost/python.hpp>
+namespace bp = boost::python;
+
+#include <TurksHead/TurksHead.hpp>
+
+BOOST_PYTHON_MODULE( _turkshead ) {
+/*    bp::class_< FroberWrap, boost::noncopyable >( "Frober" )
+        .def( "frob", bp::pure_virtual( &Frober::frob ) )
+    ;
+    bp::class_< VerticalFrober, bp::bases< Frober > >( "VerticalFrober", bp::init< int >() );
+    bp::class_< HorizontalFrober, bp::bases< Frober > >( "HorizontalFrober" );
+
+    bp::def( "superFrobP", superFrobP );
+    bp::def( "superFrobR", superFrobR );
+
+    bp::class_< SuperFrober >( "SuperFrober", bp::init< Frober* >() )
+        .def( "refrob", &SuperFrober::refrob )
+    ;*/
+}
