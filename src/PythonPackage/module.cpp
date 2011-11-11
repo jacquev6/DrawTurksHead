bc363b1705d2562f2c2a545d5b1d487cbedcc8c3
Vincent Jacques <vincent@vincent-jacques.net>
2011-11-11 10:47:08 +0100

    Remove namespace/class ambiguities
    (not fully identified, but depends on gcc's version...)

--- src/PythonPackage/module.cpp
+++ src/PythonPackage/module.cpp
@@ -14,30 +14,30 @@ static Pycairo_CAPI_t* Pycairo_CAPI;
 
 namespace {
 
-class PythonTurksHead : public TurksHead::TurksHead, public boost::python::wrapper< TurksHead::TurksHead > {
+class PythonTurksHead : public ::TurksHead::TurksHead, public boost::python::wrapper< ::TurksHead::TurksHead > {
 public:
     PythonTurksHead( int leads, int bights, double innerRadius, double outerRadius, double lineWidth ) :
-        TurksHead::TurksHead( leads, bights, innerRadius, outerRadius, lineWidth ),
+        ::TurksHead::TurksHead( leads, bights, innerRadius, outerRadius, lineWidth ),
         pythonComputeColorHsv( this->get_override( "" ) ),
         pythonComputeColorHsvInit( false )
     {
     }
 
-    PythonTurksHead( const TurksHead::TurksHead& t ) :
-        TurksHead::TurksHead( t ),
+    PythonTurksHead( const ::TurksHead::TurksHead& t ) :
+        ::TurksHead::TurksHead( t ),
         pythonComputeColorHsv( this->get_override( "" ) ),
         pythonComputeColorHsvInit( false )
     {
     }
 
     void draw( boost::python::object context ) {
-        TurksHead::draw( Cairo::RefPtr< Cairo::Context >( new Cairo::Context( PycairoContext_GET( context.ptr() ) ) ) );
+        ::TurksHead::TurksHead::draw( Cairo::RefPtr< Cairo::Context >( new Cairo::Context( PycairoContext_GET( context.ptr() ) ) ) );
     }
 
     double getAltitude( int k, int theta ) {
-        return TurksHead::TurksHead::getAltitude(
-            TurksHead::TurksHead::Path( k ),
-            TurksHead::TurksHead::Theta( theta )
+        return ::TurksHead::TurksHead::getAltitude(
+            ::TurksHead::TurksHead::Path( k ),
+            ::TurksHead::TurksHead::Theta( theta )
         );
     }
 
@@ -54,7 +54,7 @@ private:
             double v = boost::python::extract< double >( r[2] );
             return boost::make_tuple( h, s, v );
         } else {
-            return TurksHead::TurksHead::computeColorHsv( k, theta );
+            return ::TurksHead::TurksHead::computeColorHsv( k, theta );
         }
     }
 
@@ -71,9 +71,9 @@ BOOST_PYTHON_MODULE( _turkshead ) {
     /// @todo Look at http://www.boost.org/doc/libs/1_43_0/libs/parameter/doc/html/python.html to add named parameters to the constructor
     boost::python::class_< PythonTurksHead >( "TurksHead", boost::python::init< int, int, double, double, double >() )
         .def( "draw", &PythonTurksHead::draw )
-        .add_property( "p", &TurksHead::TurksHead::getP )
-        .add_property( "q", &TurksHead::TurksHead::getQ )
-        .add_property( "d", &TurksHead::TurksHead::getD )
+        .add_property( "p", &::TurksHead::TurksHead::getP )
+        .add_property( "q", &::TurksHead::TurksHead::getQ )
+        .add_property( "d", &::TurksHead::TurksHead::getD )
         .def( "getAltitude", &PythonTurksHead::getAltitude )
     ;
 }
