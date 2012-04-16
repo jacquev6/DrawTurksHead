795246b50aa1992ffd7cac731ae00b8b6ccd1bba
Vincent Jacques <vincent@vincent-jacques.net>
2012-04-16 19:38:50 +0100

    Put DjangoWebsite in vincent-jacques.net.git

--- DjangoWebsite/manage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-sys.path.append( "../../DrawTurksHead/build/darwin_gcc_debug/pyd" )
-
-import django.core.management
-import settings
-
-django.core.management.execute_manager( settings )
