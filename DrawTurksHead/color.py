d4357dce9540a8eb49dfb9331040d8a9a3cd521a
Vincent Jacques <vincent@vincent-jacques.net>
2015-05-06 17:51:50 +0200

    Fix bug when hsv_to_rgb is called with integers
    It's more a bug in the tests because real use cases are with floats.

--- DrawTurksHead/color.py
+++ DrawTurksHead/color.py
@@ -7,7 +7,7 @@ import unittest
 
 def hsv_to_rgb(h, s, v):
     hi = int(h / 60)
-    f = h / 60 - hi;
+    f = h / 60. - hi;
     hi %= 60;
     p = v * (1 - s);
     q = v * (1 - f * s);
@@ -29,35 +29,35 @@ def hsv_to_rgb(h, s, v):
 
 class HsvToRgbTestCase(unittest.TestCase):
     def test_red(self):
-        self.assertEqual(hsv_to_rgb(0, 1, 1), (1, 0, 0))
+        self.assertEqual(hsv_to_rgb(0., 1., 1.), (1, 0, 0))
 
     def test_yellow(self):
-        self.assertEqual(hsv_to_rgb(60, 1, 1), (1, 1, 0))
+        self.assertEqual(hsv_to_rgb(60., 1., 1.), (1, 1, 0))
 
     def test_green(self):
-        self.assertEqual(hsv_to_rgb(120, 1, 1), (0, 1, 0))
+        self.assertEqual(hsv_to_rgb(120., 1., 1.), (0, 1, 0))
 
     def test_cyan(self):
-        self.assertEqual(hsv_to_rgb(180, 1, 1), (0, 1, 1))
+        self.assertEqual(hsv_to_rgb(180., 1., 1.), (0, 1, 1))
 
     def test_blue(self):
-        self.assertEqual(hsv_to_rgb(240, 1, 1), (0, 0, 1))
+        self.assertEqual(hsv_to_rgb(240., 1., 1.), (0, 0, 1))
 
     def test_magenta(self):
-        self.assertEqual(hsv_to_rgb(300, 1, 1), (1, 0, 1))
+        self.assertEqual(hsv_to_rgb(300., 1., 1.), (1, 0, 1))
 
     def test_blacks(self):
-        self.assertEqual(hsv_to_rgb(30, 1, 0), (0, 0, 0))
-        self.assertEqual(hsv_to_rgb(210, 1, 0), (0, 0, 0))
+        self.assertEqual(hsv_to_rgb(30., 1., 0.), (0, 0, 0))
+        self.assertEqual(hsv_to_rgb(210., 1., 0.), (0, 0, 0))
 
     def test_whites(self):
-        self.assertEqual(hsv_to_rgb(30, 0, 1), (1, 1, 1))
-        self.assertEqual(hsv_to_rgb(210, 0, 1), (1, 1, 1))
+        self.assertEqual(hsv_to_rgb(30., 0., 1.), (1, 1, 1))
+        self.assertEqual(hsv_to_rgb(210., 0., 1.), (1, 1, 1))
 
     def test_greys(self):
-        self.assertEqual(hsv_to_rgb(30, 0, 0.5), (0.5, 0.5, 0.5))
-        self.assertEqual(hsv_to_rgb(210, 0, 0.5), (0.5, 0.5, 0.5))
+        self.assertEqual(hsv_to_rgb(30., 0., 0.5), (0.5, 0.5, 0.5))
+        self.assertEqual(hsv_to_rgb(210., 0., 0.5), (0.5, 0.5, 0.5))
 
     def test_grey_ishes(self):
-        self.assertEqual(hsv_to_rgb(30, 0.5, 0.5), (0.5, 0.25, 0.25))
-        self.assertEqual(hsv_to_rgb(210, 0.5, 0.5), (0.25, 0.5, 0.5))
+        self.assertEqual(hsv_to_rgb(30., 0.5, 0.5), (0.5, 0.375, 0.25))
+        self.assertEqual(hsv_to_rgb(210., 0.5, 0.5), (0.25, 0.375, 0.5))
